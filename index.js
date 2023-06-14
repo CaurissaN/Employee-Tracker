d0ae99cf7bb91128792581c5a439cb46aa779afa
CaurissaN <caurissanev@gmail.com>
2023-06-14 11:24:44 -0400

    updated to import and set up basic structure

---
+++ index.js
@@ -0,0 +1,33 @@
+import  inquirer from "inquirer";
+import { createConnection } from 'mysql2';
+
+const db = createConnection({
+    host: "localhost",
+    user: "root",
+    password: "onethreefive",
+    database: "employee_db"
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    mainMenu()
+})
+
+const mainMenu = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: ["View All Employees", "Add New Employee", "View All Roles", "Add New Roles","View All Departments", "Add New Departments", "Update Employee Role"]
+        }
+    ]).then((res) => {
+        if(res.action === "View All Employees") {
+            viewAllEmployees()
+        }
+    })
+}
+
+const viewAllEmployees = () => {
+    console.log("working")
+}
